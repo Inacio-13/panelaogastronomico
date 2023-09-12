bd7cb2741671c9aa91f40beb9c6d6ec36f48d75f
Inacio-13 <144730196+Inacio-13@users.noreply.github.com>
2023-09-11 21:32:31 -0300

    commit inicial
    primeiro commit do site

---
+++ php/conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'usbw';
+    $dbName = 'formulario-panelao';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    
+?>
